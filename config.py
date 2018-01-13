618fc8fa045926e7d947135b81368e76587c721a
parker <rrdssfgcs@163.com>
2018-01-13 12:21:28 +0800

    MOD:添加百度SDK。

--- config.py
+++ config.py
@@ -2,10 +2,24 @@
 
 data_directory = "screenshots"
 
-hanwan_appcode = "3cc4f16c357e4f329dab5e71c810a871"
-
 vm_name = '夜神模拟器'
 
 app_name = '西瓜视频百万英雄'
 
 search_engine = 'http://www.baidu.com'
+
+hot_key = 'F2'
+
+ocr_engine = 'baidu'
+
+### baidu orc
+app_id = "10684437" # 改成自己的哦
+app_key = "85zhxabAYUfaggRpjG5Gxcct" # 改成自己的哦
+app_secret = "hFZ92T4H8vrIcTSSdRmEo6E5vhTiFN6d " # 改成自己的哦
+
+### 0 表示普通识别
+### 1 表示精确识别
+api_version = 1
+
+### hanwang orc
+hanwan_appcode = "3cc4f16c357e4f329dab5e71c810a871" # 改成自己的哦
--- main.py
+++ main.py
@@ -6,18 +6,19 @@
     问答助手~
 
 """
-import textwrap
 import time
-from argparse import ArgumentParser
-from pyhooked import Hook, KeyboardEvent, MouseEvent
 import win32gui
+from argparse import ArgumentParser
 
-from config import data_directory, hanwan_appcode,app_name,vm_name,search_engine
-from core.windows import analyze_current_screen_text
-from core.hanwanocr import get_text_from_image
-
+from pyhooked import Hook, KeyboardEvent
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+from config import data_directory, hanwan_appcode, vm_name, search_engine, hot_key, ocr_engine, app_id, app_key, \
+    app_secret, api_version
+from core.ocr import get_text_from_image_hanwang, get_text_from_image_baidu
+from core.windows import analyze_current_screen_text
+
 
 def parse_args():
     parser = ArgumentParser(description="Assistant")
@@ -37,28 +38,30 @@ def main():
 
     start = time.time()
     text_binary = analyze_current_screen_text(
-    	label=vm_name,
+        label=vm_name,
         directory=data_directory
     )
-    keyword = get_text_from_image(
-        image_data=text_binary,
-        appcode=hanwan_appcode
-    )
+    if ocr_engine == 'baidu':
+        print("用百度去OCR识别了!!!\n")
+        keyword = get_text_from_image_baidu(image_data=text_binary, app_id=app_id, app_key=app_key,
+                                            app_secret=app_secret, api_version=api_version, timeout=5)
+    else:
+        print("用汉王去OCR识别了!!!\n")
+        keyword = get_text_from_image_hanwang(image_data=text_binary, appcode=hanwan_appcode)
+
     if not keyword:
         print("没识别出来，随机选吧!!!\n")
         print("题目出现的时候按F2，我就自动帮你去搜啦~\n")
         return
 
-    keyword = keyword.split(r"．")[-1]
-    keywords = keyword.split(" ")
-    keyword = "".join([e.strip("\r\n") for e in keywords if e])
-    keyword.replace('\n',' ')
+    keyword = "".join([e.strip("\r\n") for e in keyword if e])
+    keyword.replace('\n', ' ')
     if len(keyword) < 2:
         print("没识别出来，随机选吧!!!\n")
         print("题目出现的时候按F2，我就自动帮你去搜啦~\n")
         return
-    print("我用关键词:\" ", keyword,"\"去百度答案啦!")
-    
+    print("我用关键词:\" ", keyword, "\"去百度答案啦!")
+
     elem = browser.find_element_by_id("kw")
     elem.clear()
     elem.send_keys(keyword)
@@ -67,9 +70,10 @@ def main():
     print("结果在浏览器里啦~\n")
     print("题目出现的时候按F2，我就自动帮你去搜啦~\n")
 
+
 def handle_events(args):
     if isinstance(args, KeyboardEvent):
-        if args.current_key == 'F2' and args.event_type == 'key down':
+        if args.current_key == hot_key and args.event_type == 'key down':
             main()
         elif args.current_key == 'Q' and args.event_type == 'key down':
             hk.stop()
@@ -82,10 +86,8 @@ if __name__ == "__main__":
     hld = win32gui.FindWindow(None, vm_name)
     if hld > 0:
         print('用模拟器打开对应应用~~\n题目出现的时候按F2，我就自动帮你去搜啦~\n')
-        hk = Hook()  
-        hk.handler = handle_events  
-        hk.hook()  
+        hk = Hook()
+        hk.handler = handle_events
+        hk.hook()
     else:
-        print('咦，你没打开'+vm_name+'吧!')    
-  
-
+        print('咦，你没打开' + vm_name + '吧!')
